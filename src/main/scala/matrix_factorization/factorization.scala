3f6ed7dca44ea61065e050b2b65dc52805a8e500
akshaykulkarni <kulkarni.akshay@husky.neu.edu>
2019-11-22 13:07:39 -0500

    Update factorization.scala

--- src/main/scala/matrix_factorization/factorization.scala
+++ src/main/scala/matrix_factorization/factorization.scala
@@ -17,8 +17,6 @@ import org.apache.spark.sql.SparkSession
 
 object factorization {
 
-  // Initialize k, number of iterations, and number of total vertices in synthetic graph
-
   val n_factors = 10;
   val n_iterations = 10
 
@@ -43,44 +41,6 @@ object factorization {
     // Define partitioner
     val partitioner = new HashPartitioner(1)
 
-    // Create synthetic graph
-    val synthetic_graph = sc.parallelize(generateGraph(k)).partitionBy(partitioner)
-
-    // Create an adjacency list RDD with (key, adj_list val) pairs
-    val links  = synthetic_graph.groupByKey()
-    var page_ranks = links.mapValues(node => (1.0 / (vertices)))
-
-    // Compute sink node rank for dangling nodes and set its page rank score to to 0.
-    // Add the sink node to the page ranks RDD
-    val sink_node_rank = sc.parallelize(List((0, 0.0))).partitionBy(partitioner)
-    page_ranks = page_ranks.union(sink_node_rank)
-
-    //Iterate through the graph
-    for (itr_count <- 1 to iterations){
-
-        // Compute the incoming contributions for all nodes that receive in-links and aggregate the PR value per node
-        val inc_contribs = links.join(page_ranks)
-          .flatMap{case(pageID, (links, rank)) => links.map(page => (page, rank/links.size))}
-          .reduceByKey(_+_)
-
-        // Compute and collect the sink nodes mass from all incoming contributions into sink node
-        val sink_node_mass = inc_contribs.lookup(0)(0)
-
-        // Distribute the mass evenly among all nodes
-        val mass_distribution = sink_node_mass / (vertices)
-
-        // Compute final contributions, filtering out the sink node
-        val full_contributions = page_ranks
-          .leftOuterJoin(inc_contribs)
-          .filter(_._1 != 0)
-          .mapValues{case(x,y) => if (y.isDefined) y.get else 0.0 }
-
-        // Update the page rank values for current iteration
-        page_ranks = full_contributions.mapValues(v => 0.15/(vertices) + 0.85*(v + mass_distribution)).persist()
-    }
-
-    page_ranks.sortBy(_._1, true).saveAsTextFile(args(0))
-    logger.info(page_ranks.toDebugString)
 
         }
 }
