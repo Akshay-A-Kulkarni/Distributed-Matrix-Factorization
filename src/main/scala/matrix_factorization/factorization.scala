0603244acf71adf5a8223045cadf3e9e6e747e8b
akshay-a-kulkarni <kulkarni.akshay@husky.neu.edu>
2019-11-24 02:06:34 -0500

    added preliminary user & item link preprocessing fucntion

--- src/main/scala/matrix_factorization/factorization.scala
+++ src/main/scala/matrix_factorization/factorization.scala
@@ -2,13 +2,14 @@ package matrix_factorization
 
 import org.apache.log4j.LogManager
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 
 object factorization {
 
   val n_factors = 10;
-  val n_iterations = 10
+  val convergence_iterations = 10
 
   def main(args: Array[String]) {
 
@@ -28,11 +29,6 @@ object factorization {
       .setAppName("MatrixFactorization")
       .setMaster("local[*]")
 
-//     val spark = SparkSession
-//       .builder()
-//       .config(conf)
-//       .getOrCreate()
-
     val mySpark = SparkSession
       .builder()
       .config(conf)
@@ -45,10 +41,59 @@ object factorization {
     val sc = mySpark.sparkContext
 
 
-    var RDD_dataset = sc.textFile("input/small.txt")
-      .map(data => data.split(","))
-      .map(data => (data(0).toInt, data(1).toInt, data(2).toInt))
+    val inputRDD = sc.textFile("input/small.txt")
+                          .map{
+                            line => {
+                              val list = line.split(",")
+                              (list(0).toInt, (list(1).toInt, list(2).toInt))
+                            }
+                          }
 
-    RDD_dataset.foreach(println)
+    // Link information for users and items
+
+    val user_blocks = getBlocks("user",inputRDD)
+    val item_blocks = getBlocks("item",inputRDD)
+
+    // initialising Factor matrices
+
+    val P = user_blocks.mapValues{
+                         val rand = scala.util.Random
+                         v => rand.nextFloat()
+    }
+
+    val Q = item_blocks.mapValues{
+                          val rand = scala.util.Random
+                          v => rand.nextFloat()
+    }
+
+    P.foreach(println)
+    Q.foreach(println)
+
   }
-}
+
+  def getBlocks(bType: String, R : RDD[(Int,(Int,Int))]): RDD[(Int,Iterable[Int])] = {
+    /*
+    @params
+    bType : Str -> block type  ("user"/"item")
+    R     : RDD -> initial ratings RDD
+
+    The function takes in the input RDD to assign contiguous indices to users and items and computes the
+    user-item and item-user links.
+
+    (note : index functionality incomplete)
+    */
+
+    bType match {
+                  case "user"  => {
+                                    val userBlocks = R.map{ case (u,(i,v)) => (u,i) }.groupByKey()
+                                    return userBlocks
+                  }
+                  case "item"  => {
+                                    val itemBlocks= R.map{ case (u,(i,v)) => (u,i) }.map(x => x.swap).groupByKey()
+                                    return itemBlocks
+                  }
+
+    }
+  }
+
+  }
