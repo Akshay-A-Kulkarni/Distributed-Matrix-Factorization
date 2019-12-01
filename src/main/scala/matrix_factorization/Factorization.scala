458af313090796d9255f76b9be550dd9c604f602
akshay-a-kulkarni <kulkarni.akshay@husky.neu.edu>
2019-12-01 13:40:33 -0500

    fixed randomization rating range for factor matrices+cleanup

--- src/main/scala/matrix_factorization/Factorization.scala
+++ src/main/scala/matrix_factorization/Factorization.scala
@@ -1,16 +1,18 @@
 package matrix_factorization
 
 import org.apache.log4j.LogManager
-import org.apache.spark.SparkConf
+import org.apache.spark.{HashPartitioner, SparkConf}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 
-object factorization {
+object Factorization {
 
-  val nFactors = 10
-  val convergenceIterations = 10
-  val seedVal = 123
+  val nFactors  : Int = 10
+  val seedVal   : Int = 123
+  val minRating : Int = 1
+  val maxRating : Int   = 5
+  val convergenceIterations : Int = 10
 
   def main(args: Array[String]) {
 
@@ -26,27 +28,27 @@ object factorization {
     // ================
 
     val conf = new SparkConf()
-      .setAppName("MatrixFactorization")
-      .setMaster("local[*]")
+                              .setAppName("MatrixFactorization")
+                              .setMaster("local[*]")
 
-    val mySpark = SparkSession
-      .builder()
-      .config(conf)
-      .getOrCreate()
+    val spark = SparkSession.builder()
+                            .config(conf)
+                            .getOrCreate()
 
     // For implicit conversions like converting RDDs to DataFrames
-    import mySpark.implicits._
+    import spark.implicits._
 
-    val sc = mySpark.sparkContext
+    val sc = spark.sparkContext
+
+    val partitioner = new HashPartitioner(1)
 
     val inputRDD = sc.textFile("input/small.txt")
       .map { line => {
         val list = line.split(",")
         (list(0).toInt, (list(1).toInt, list(2).toInt))
         }
-      }
+      }.partitionBy(partitioner)
 
-    val rand = scala.util.Random
 
     val sortedUsers = sortByRelativeIndex("user", inputRDD)
     val sortedItems = sortByRelativeIndex("item", inputRDD)
@@ -55,22 +57,31 @@ object factorization {
     val user_blocks = getBlocks("user", inputRDD, sortedUsers, sortedItems)
     val item_blocks = getBlocks("item", inputRDD, sortedUsers, sortedItems)
 
+    // Creating two Ratings Matrices partitioned by user and item respectively
+    val R_u = inputRDD.cache()
+    val R_i = inputRDD.map(i => (i._2._1, (i._1,i._2._2))).partitionBy(partitioner).cache()
+
     // initialising random Factor matrices
-    val P = user_blocks.mapPartitionsWithIndex { (idx, iter) =>
+    val P = user_blocks.mapPartitionsWithIndex { (idx, row) =>
       val rand = new scala.util.Random(idx + seedVal)
-      iter.map(x => (x._1, Seq.fill(nFactors)(rand.nextInt(5))))
-    }
+      row.map(x => (x._1, Seq.fill(nFactors)(minRating + rand.nextInt((maxRating - minRating) + 1 ))))
+    }.collect()
 
-    val Q = item_blocks.mapPartitionsWithIndex { (idx, iter) =>
+    val Q = item_blocks.mapPartitionsWithIndex { (idx, row) =>
       val rand = new scala.util.Random(idx + seedVal)
-      iter.map(x => (x._1, Seq.fill(nFactors)(rand.nextInt(5))))
-    }
+      row.map(x => (x._1, Seq.fill(nFactors)(minRating + rand.nextInt((maxRating - minRating) + 1 ))))
+    }.collect()
 
-    user_blocks.foreach(println)
-    item_blocks.foreach(println)
 
-        P.foreach(println)
-        Q.foreach(println)
+//    val U = sc.broadcast(P)
+//    val M = sc.broadcast(Q)
+
+    //    sortedUsers.foreach(println)
+//    user_blocks.foreach(println)
+//    item_blocks.foreach(println)
+//
+//    R_i.foreach(println)
+//    Q.foreach(println)
   }
 
   def sortByRelativeIndex(bType: String, input: RDD[(Int, (Int, Int))]): Array[(Int, Long)] = {
@@ -79,7 +90,7 @@ object factorization {
     bType : Str -> block type  ("user"/"item")
     R     : RDD -> initial ratings RDD
 
-    The function takes in the input RDD to assign each unique user/item ID to a relative index to a relative index
+    The function takes in the input RDD to assign each unique user/item ID to a relative index
     and sorts list in ascending order.
 
     */
@@ -120,7 +131,8 @@ object factorization {
       .head
   }
 
-  def getBlocks(bType: String, R: RDD[(Int, (Int, Int))], sortedUsers: Array[(Int, Long)], sortedItems: Array[(Int, Long)]): RDD[(Long, Iterable[Long])] = {
+  def getBlocks(bType: String, R: RDD[(Int, (Int, Int))], sortedUsers: Array[(Int, Long)],
+                sortedItems: Array[(Int, Long)]): RDD[(Long, Iterable[Long])] = {
     /*
     @params
     bType : Str -> block type  ("user"/"item")
@@ -132,14 +144,15 @@ object factorization {
     */
     bType match {
       case "user" => {
-        val userBlocks = R.map { case (u, (i, v)) => (getRelativeIndex(u, sortedUsers), getRelativeIndex(i, sortedItems)) }.groupByKey()
+        val userBlocks = R.map {case (u, (i, v)) => (getRelativeIndex(u, sortedUsers), getRelativeIndex(i, sortedItems))
+                                  }.groupByKey()
         return userBlocks
       }
       case "item" => {
-        val itemBlocks = R.map { case (u, (i, v)) => (getRelativeIndex(i, sortedItems), getRelativeIndex(u, sortedUsers)) }.groupByKey()
+        val itemBlocks = R.map {case (u, (i, v)) => (getRelativeIndex(i, sortedItems), getRelativeIndex(u, sortedUsers))
+                                  }.groupByKey()
         return itemBlocks
       }
     }
   }
-
 }
